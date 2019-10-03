ddc9df245ad9fda20847fb87b977d443137680ae
root <root@ip-172-31-46-12.ec2.internal>
2019-10-03 10:42:36 +0000

    test

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ $name = "hello";
 <h1 ALIGN=CENTER> <?php include '/home/ubuntu/jenkins/website/config.php'; echo $ip;  ?> <?php include '/home/ubuntu/jenkins/website/config.php';
 echo $va;
 ?></h1>
-<img src="/images/github3.jpg" width="280" height="125" title="Logo of a company" alt="Logo of a company" />
-
+<?php
+<h2 ALLGN=CENTER><img src="images/github3.jpg" width="300" height="300" /></h2>
+?>
 
 </body>
 </html>
