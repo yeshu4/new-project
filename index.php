a845c79d68f7f919bfc6c4c625b38b5a07f30fa6
root <root@ip-172-31-46-12.ec2.internal>
2019-10-03 09:08:57 +0000

    trying new

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ $name = "hello";
 <h1 ALIGN=CENTER> <?php include '/home/ubuntu/jenkins/website/config.php'; echo $ip;  ?> <?php include '/home/ubuntu/jenkins/website/config.php';
 echo $va;
 ?></h1>
+<img src="/images/github3.jpg" width="280" height="125" title="Logo of a company" alt="Logo of a company" />
+
 
 </body>
 </html>
