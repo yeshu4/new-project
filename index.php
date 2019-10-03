c303d9b24ce9c3c3e4aef0cb6e63b2aadfc8ad48
root <root@ip-172-31-46-12.ec2.internal>
2019-10-03 08:42:27 +0000

    changed to php

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+$name = "hello";
+?>
+<html>
+<body style = "background-image:url('images/github3.jpg'); background-size: 100%">
+<h1 ALIGN=CENTER> <?php echo $GLOBALS['name'];  ?> <?php include '/home/ubuntu/jenkins/website/config.php';
+echo $va;
+?></h1>
+
+</body>
+</html>
