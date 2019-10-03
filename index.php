7e24a1455c9bc45d273e9887186a259a8d33cab8
root <root@ip-172-31-46-12.ec2.internal>
2019-10-03 10:48:13 +0000

    test

--- index.php
+++ index.php
@@ -3,13 +3,10 @@
 $name = "hello";
 ?>
 <html>
-<body>
+<body style = "background-image:url('images/github3.jpg'); background-size: 100%">
 <h1 ALIGN=CENTER> <?php include '/home/ubuntu/jenkins/website/config.php'; echo $ip;  ?> <?php include '/home/ubuntu/jenkins/website/config.php';
 echo $va;
 ?></h1>
-<?php
-<h2 ALLGN=CENTER><img src="./images/github3.jpg" width="300" height="300" /></h2>
-?>
 
 </body>
 </html>
